31768513e8ec73063b346117f8b2c15336d0f60d
timothyphilip <philippetimothe@gmail.com>
2020-11-04 13:20:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+let element = document.querySelector("main#main");
+    element.remove()
+
+let newHeader = document.createElement('h1')
+
+
+
+
+    // newHeader.appendChild('h1#victory')
+    newHeader.id = "victory"
+    newHeader.innerHTML = "<h1>is the champion</h1>"
