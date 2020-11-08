4efb102a98ab3645d533e1e999ef1b44c881e554
chica25 <patricia@patriciaco.com>
2020-11-08 03:05:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 // Write your code here!
 
 
-// 1) no longer has DOM node 'main#main'
-// document.querySelector('main#main').remove();
-main.remove();
+// 1) no longer has DOM node 'main#main
 
-// 2) has a 'newHeader' variable that points to node 'h1#victory'
+main.remove();
+// main.removeParent(main#main)
+
+
+//2) has a 'newHeader' variable that points to node 'h1#victory'
+
+
 const newHeader = document.createElement('h1');
 
 // 3) has a 'newHeader' variable that points to node 'h1#victory'
+
+
 newHeader.id = 'victory'
 
 //has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside:
-// newHeader.innerHTML = 'Patricia is the champion';
-newHeader.innerHTML = `{YOUR-NAME} is the champion`;
-newHeader.className = 'victory';
+newHeader.innerHTML = 'Patricia is the champion';
+
